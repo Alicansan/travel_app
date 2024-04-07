a838c100d3bae5a8a452c3f2e584418da2a2b461
Alican Şan <alicansan2606@gmail.com>
2024-04-07 18:07:02 +0300

    as

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,10 +1,25 @@
-
+const CampSite = ()=>{
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
 const Camp = () => {
   return (
-    <div>
-      Camp
-    </div>
+    <section className='border-2 border-red-500 
+    2xl:max-container relative-flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
+      <div className='hide-scrollbar border-2 border-blue-500 flex 
+      h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
+      <CampSite 
+        backgroundImage= "bg-bg-img-1"
+        title="Putuk Truno Camp"
+        subtitle="Prigen, Pasuruan"
+        peopleJoined="50+joined"
+      />
+      </div>
+    </section>
   )
 }
 
